c4786e7b14566b56b09198dfdae4b793a3a5c76e
nuna18t <nochu2c4s@gmail.com>
2023-09-10 16:16:48 +0300

    hello world
    hello world

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream> //i/o library
+using namespace std;// instead prefix std
+
+
+int main() //header
+{ //begin
+
+	cout << "Hello world!" << endl;
+
+	system("pause");
+
+	return 0;//return to os
+
+}//end
